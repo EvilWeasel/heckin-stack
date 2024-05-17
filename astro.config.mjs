12a5fb445b103e292f4a0d2391ffeab581ac9ca0
sequel.weasel <sequel.weasel@gmail.com>
2024-05-17 18:29:42 +0000

    fixed baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://evilweasel.github.io',
+  base: '/heckin-stack',
   integrations: [mdx(), sitemap(), tailwind()]
 });
